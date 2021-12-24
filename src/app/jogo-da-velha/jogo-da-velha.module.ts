5a8e82f356934c67a728380382539b5ac407cdf3
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-24 18:39:23 -0300

    criado 5 arquivo do novo component do jogo

--- src/app/jogo-da-velha/jogo-da-velha.module.ts
+++ src/app/jogo-da-velha/jogo-da-velha.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { JogoDaVelhaComponent } from './jogo-da-velha.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    JogoDaVelhaComponent
+  ],
   imports: [
     CommonModule
   ]
