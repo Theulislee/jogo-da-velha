c5f58826210df1b19af477b4d3b6c3baf26043b0
Matheus Magalhães <magalhaescerqueiraesilva@gmail.com>
2021-12-26 19:59:31 -0300

    add serviço com atributos do jogo

--- src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
+++ src/app/jogo-da-velha/shared/jogo-da-velha.service.ts
@@ -5,5 +5,20 @@ import { Injectable } from '@angular/core';
 })
 export class JogoDaVelhaService {
 
+  //Add serviço da calculadora.
+  private readonly TAM_TAB: number = 3;
+  private readonly X: number = 1;
+  private readonly O: number = 2;
+  private readonly VAZIO: number = 0;
+
+  private tabuleiro: any;
+  private numMovimentos: number;
+  private vitoria: any;
+
+  private _jogador: number;
+  private _showInicio: boolean;
+  private _showTabuleiro: boolean;
+  private _showFinal: boolean;
+
   constructor() { }
 }
